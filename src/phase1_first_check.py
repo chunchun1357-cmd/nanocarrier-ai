730821bbab9a1b720bf7a671ab5494df4df8b643
chunchun1357-cmd <chunchun1357@gmail.com>
2026-06-30 18:23:54 +0900

    Fix typo in environment message

--- src/phase1_first_check.py
+++ src/phase1_first_check.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-print("Miniconda environmnet works")
+print("Miniconda environment works")
 
 x = np.array([[1], [2], [3], [4], [5]])
 y = np.array([2, 4, 6, 8, 10])
